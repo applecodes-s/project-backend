776356b59f156307a7fbcf4bf61fc5fb3ceca061
Priya <spriyakeziah@gmail.com>
2025-07-19 18:01:07 +0530

    cleaned

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const projectRoutes = require('./routes/projectRoutes'); // ✅ Make sure this points to the right JS file
+const projectRoutes = require('./routes/projectRoutes'); 
 dotenv.config();
 
 const app = express();
